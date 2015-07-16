078597ddc1ea15c2b5117fc90448645274eefb56
Arjan Snaterse <arjan@uprise.nl>
2015-07-16 11:26:37 +0200

    Auto tab

--- webroot/wp-content/themes/keuze/templates/forms/auto.php
+++ webroot/wp-content/themes/keuze/templates/forms/auto.php
@@ -1,20 +1,20 @@
-<form action="" method="get">
+<form action="<?php echo get_home_url( null, 'autoverzekering/' ); ?>#vergelijken" method="get">
     <p>
         <label for="auto-kenteken">Je kenteken</label>
-        <input type="text" id="auto-kenteken" name="kenteken" class="kenteken">
+        <input type="text" id="auto-kenteken" name="f[licenseplate]" class="kenteken">
         <span class="description"><a href="#">Ik weet mijn kenteken niet</a></span>
     </p>
     <p>
         <label for="auto-postcode">Je postcode</label>
-        <input type="text" id="auto-postcode" name="postcode" class="postcode">
+        <input type="text" id="auto-postcode" name="f[postal_code]" class="postcode">
     </p>
     <p>
         <label for="auto-geboortedatum">Je geboortedatum</label>
-        <input type="text" id="auto-geboortedatum" name="geboortedatum" class="geboortedatum">
+        <input type="text" id="auto-geboortedatum" name="f[birthdate]" class="geboortedatum" placeholder="24-01-1979">
     </p>
     <p>
         <label for="auto-schadevrij">Schadevrije jaren</label>
-        <select id="auto-schadevrij" name="schadevrij" class="schadevrij">
+        <select id="auto-schadevrij" name="f[years_without_damage]" class="schadevrij">
             <option value="0">-2</option>
             <option value="0">-1</option>
             <option value="0" selected="selected">0</option>
